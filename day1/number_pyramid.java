831b2443a13940d0cb9b020bd842012d4952cbd9
raji024 <rajeshwariak7@gmail.com>
2025-05-25 18:57:43 +0530

    finish it

--- day1/number_pyramid.java
+++ day1/number_pyramid.java
@@ -5,7 +5,14 @@ public class number_pyramid {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
         for(int i=1;i<=x;i++){
-            
-        }
+            for(int j=1;j<=x-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=i;j++){
+               System.out.print(j+" ");
+            }
+            System.out.println();
     }
+}
 }
