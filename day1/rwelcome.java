35a4df6d62b152708bcc58adfad3755a8dd62ac2
raji024 <rajeshwariak7@gmail.com>
2025-05-25 17:44:55 +0530

    left_align completed

---
+++ day1/rwelcome.java
@@ -0,0 +1,9 @@
+package day1;
+import java.util.*;
+public class rwelcome {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+    }
+}
---
+++ day1/triangle.java
@@ -0,0 +1,30 @@
+package day1;
+import java.util.*;
+public class triangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=x-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=x-1;i>=1;i--){
+            for(int j=1;j<=x-i;j++){
+                System.out.print(" ");
+
+            }
+            for(int j=1;j<=2*i-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
