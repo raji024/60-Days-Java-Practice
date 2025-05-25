5563628e5b27bc7fd73fb4111d7bdab8e8208847
raji024 <rajeshwariak7@gmail.com>
2025-05-25 18:53:45 +0530

    runned it

---
+++ day1/number_pyramid.java
@@ -0,0 +1,11 @@
+package day1;
+import java.util.*;
+public class number_pyramid {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=1;i<=x;i++){
+            
+        }
+    }
+}
---
+++ day1/traingle2.java
@@ -0,0 +1,17 @@
+package day1;
+import java.util.*;
+public class traingle2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=x-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
