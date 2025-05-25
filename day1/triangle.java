6247253e2debc4084895509d643638af0ea3c5da
raji024 <rajeshwariak7@gmail.com>
2025-05-25 17:49:53 +0530

    runned it

--- day1/triangle.java
+++ day1/triangle.java
@@ -6,22 +6,13 @@ public class triangle {
         int x = sc.nextInt();
         for(int i=1;i<=x;i++){
             for(int j=1;j<=x-i;j++){
-                System.out.print(" ");
+                System.out.print(" "); //ugughug
 
             }
             for(int j=1;j<=2*i-1;j++){
                 System.out.print("*");
             }
-            System.out.println();
-        }
-        for(int i=x-1;i>=1;i--){
-            for(int j=1;j<=x-i;j++){
-                System.out.print(" ");
 
-            }
-            for(int j=1;j<=2*i-1;j++){
-                System.out.print("*");
-            }
             System.out.println();
         }
         
