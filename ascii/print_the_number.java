94d0057efa7e74039fc7d7462abc31f48becfa4d
raji024 <rajeshwariak7@gmail.com>
2025-06-08 10:42:07 +0530

    commit

--- ascii/print_the_number.java
+++ ascii/print_the_number.java
@@ -1,5 +1,9 @@
 package ascii;
-impo
+import java.util.*;
 public class print_the_number {
-    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println((char)num);
+    }
 }
---
+++ ascii/uppercase_or_lowercase.java
@@ -0,0 +1,8 @@
+package ascii;
+import java.util.*;
+public class uppercase_or_lowercase {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        char ch = 
+    }
+}
