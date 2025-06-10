0dd863a1d65756a12328b23be9ae669a1d67440e
raji024 <rajeshwariak7@gmail.com>
2025-06-10 18:12:01 +0530

    finished

---
+++ day13/ascii/print_char_from_65_to_90.java
@@ -0,0 +1,12 @@
+package day13.ascii;
+import java.util.*;
+public class print_char_from_65_to_90 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        for(int i=65;i<=90;i++){
+            char ch = (char)(i);
+            System.out.println(ch);
+        }
+    }
+
+}
