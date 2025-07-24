33b408f80e763e9ee894e7918e1668a46227e21f
raji024 <rajeshwariak7@gmail.com>
2025-07-24 21:09:40 +0530

    commit

---
+++ arrayList/impli_arrayList.java
@@ -0,0 +1,13 @@
+package arrayList;
+import java.util.*;
+public class impli_arrayList {
+    public static void main (String[] args){
+        ArrayList<Integer> n = new ArrayList<>();
+        n.add(50);
+        n.add(70);
+        n.add(40);
+        n.add(10);
+        n.add(30);
+        System.out.println(n);
+    }
+}
