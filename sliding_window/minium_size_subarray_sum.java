4bb900101f3f4b06ee4cca4540d11368a204e9a3
raji024 <rajeshwariak7@gmail.com>
2025-08-11 09:23:13 +0530

    commit

--- sliding_window/minium_size_subarray_sum.java
+++ sliding_window/minium_size_subarray_sum.java
@@ -10,13 +10,19 @@ public class minium_size_subarray_sum {
         }
         int k = sc.nextInt();
         int left =0;
+        int min = Integer.MAX_VALUE;
         int sum =0;
         for(int right=0;right<x;right++){
             sum+=arr[right];
             while(sum>=k){
-                
+                min = Math.min(min,right-left+1);
+                sum-=arr[left];
+                left++;
             }
+
         }
+        System.out.println(min);
+
 
     }
 }
