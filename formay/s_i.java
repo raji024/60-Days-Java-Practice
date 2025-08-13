fb1f6ad1340c5c4705c9961c36f9093c203333b3
raji024 <rajeshwariak7@gmail.com>
2025-08-13 15:54:21 +0530

    commit

---
+++ formay/s_i.java
@@ -0,0 +1,11 @@
+package formay;
+import java.util.*;
+public class s_i {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int b  =  Integer.parseInt(a);
+        System.out.println(b);
+
+    }
+}
