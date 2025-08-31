5c90ef702adbb82538858a0972c088cf4c55d4f3
raji024 <rajeshwariak7@gmail.com>
2025-08-31 11:29:17 +0530

    commit

---
+++ recursion/fib.java
@@ -0,0 +1,17 @@
+package recursion;
+import java.util.*;
+public class fib {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(fib(x));
+    }
+    public static int fib(int n){
+        if(n==0|| n==1){
+            return n;
+        }else{
+            return fib(n-1)+fib(n-2);
+        }
+    }
+}
+
