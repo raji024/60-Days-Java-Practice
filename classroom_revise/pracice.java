98e75212a09d0642cd965093e315c9469aac5f14
raji024 <rajeshwariak7@gmail.com>
2025-09-23 15:14:03 +0530

    commit

---
+++ classroom_revise/pracice.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class pracice {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=1;i<=x;i++){
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=x-1;i>=1;i--){
+            for(int j=1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ classroom_revise/triangle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class triangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for(int i=1;i<=x;i++){
+            for(int i=1;i<x-i;j)
+        }
+    }
+}
